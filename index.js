5e56eed5a7353dccd0c46f6c7e54bfbfcb281f5d
Creepty07 <diego.alejandrolugo07@gmail.com>
2026-03-09 17:25:52 -0600

    archivos actualizados

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const { pokemon } = require('./pokedex.json')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res, next) => {
+  res.status(200);
+  res.send("Bienvenido al Pokedex");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
+app.get('/pokemon/all', (req, res, next) => {
+  res.status(200);
+  res.send(pokemon);
+});
+
+app.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
+  const id = Number(req.params[0]); 
+  if (Number.isInteger(id) && id > 0 && id <= pokemon.length) {
+    res.status(200).send(pokemon[id - 1]);
+  } else {
+    res.status(404).send("Pokémon no encontrado");
+  }
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+  const name = req.params.name;
+  for (i = 0; i < pokemon.length; i++) {
+    if(pokemon[i].name == name) {
+      res.status(200);
+      return res.send(pokemon[i]);
+    }
+  }
+  res.status(404);
+  return res.send('Pokémon no encontrado')
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is listening at http://localhost:3000`);
 });
