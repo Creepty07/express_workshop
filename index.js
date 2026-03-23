dd87d403e015c508e17b2ba53462ff5fdd4e313a
Creepty07 <diego.alejandrolugo07@gmail.com>
2026-03-22 22:24:04 -0600

    actividad 5

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const express = require('express');
 const app = express();
 const { pokemon } = require('./pokedex.json')
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+/*
+API REST
+GET - Obtener información
+POST - Crear un nuevo recurso
+PATCH - Actualizar parcialmente un recurso existente
+PUT - Actualizar un recurso existente
+DELETE - Eliminar un recurso existente
+*/
+
 app.get('/', (req, res, next) => {
-  res.status(200);
-  res.send("Bienvenido al Pokedex");
+  res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.get('/pokemon/all', (req, res, next) => {
-  res.status(200);
-  res.send(pokemon);
+app.post('/pokemon', (req, res, next) => {
+  return res.status(201).send(req.body);
+});
+
+app.get('/pokemon', (req, res, next) => {
+  res.status(200).send(pokemon);
 });
 
 app.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
@@ -23,15 +37,14 @@ app.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
 
 app.get('/pokemon/:name', (req, res, next) => {
   const name = req.params.name;
-  for (i = 0; i < pokemon.length; i++) {
-    if(pokemon[i].name == name) {
-      res.status(200);
-      return res.send(pokemon[i]);
-    }
+  if (!/^[A-Za-zñÑ\s]+$/.test(name)) {
+    return res.status(400).send('Nombre de Pokémon no válido');
   }
-  res.status(404);
-  return res.send('Pokémon no encontrado')
-})
+  const pk = pokemon.find(p => p.name.toUpperCase() === name.toUpperCase());
+  return pk
+    ? res.status(200).send(pk)
+    : res.status(404).send('Pokémon no encontrado');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is listening at http://localhost:3000`);
